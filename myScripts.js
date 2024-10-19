93f7a07e84f3a7bcb3373be77740d3b16155a5c6
lsivilotti <luca.sivilotti@gmail.com>
2024-10-19 18:28:02 -0400

    Import file and set text to report content <p> tag

--- myScripts.js
+++ myScripts.js
@@ -1,5 +1,22 @@
 var doc = document;
 
+var fileRun = doc.getElementById("run");
+var fileRead = doc.getElementById("fileInput");
+var content;
+fileRead.addEventListener("change", (event) => {
+    var file = event.target.files[0];
+    var reader = new FileReader;
+    reader.onload = (e) => {
+        content = e.target.result;
+    };
+    reader.readAsText(file);
+
+    content = file.text();
+});
+fileRun.addEventListener("click", function() {
+    doc.getElementsByClassName("reportContent").textContent = content;
+})
+
 var title = doc.getElementsByClassName("reportTitle");
 
 for (var i = 0; i < title.length; i++) {
